0656bf289e9a476f05fcd9c872698b97f98ff2a9
abdulwahabfarooq <abdulwahabfarooq@outlook.com>
2024-01-03 17:09:51 +0500

    Updating using setter and getter
    Stored old value in a variable and then updated and printed in one line

---
+++ MappingUpdate.js
@@ -0,0 +1,35 @@
+// Define the drawingRoom constructor function
+function DrawingRoom() {
+    this.roomMap = new Map();
+  }
+  
+  // Add setter method to the prototype
+  DrawingRoom.prototype.set = function(key, value) {
+    this.roomMap.set(key, value);
+  };
+  
+  // Add getter method to the prototype
+  DrawingRoom.prototype.get = function(key) {
+    return this.roomMap.get(key);
+  };
+  
+  // Create an instance of DrawingRoom
+  let house = new DrawingRoom();
+  
+  // Set values using the setter method
+  house.set("Tables", 1);
+  house.set("Chairs", 6);
+  house.set("Sofas", 2);
+  house.set("Cupboards", 3);
+  
+  // Retrieve values using the getter method
+  let value = house.get("Tables");
+  console.log("The number of Tables in a Drawing Room is: " + value);
+  
+
+//Update the values
+
+let oldCupboards = house.get("Cupboards");
+house.set("Cupboards", 10);
+value = house.get("Cupboards");
+console.log("The number of Cupboards have been updated from: " + oldCupboards + " to: " + value);
